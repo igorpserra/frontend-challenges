32b61286172063eeff88e384353dd1e887fc5ffe
igorpserra <igorpserra@gmail.com>
2022-02-05 03:02:19 -0300

    homepage

--- sw_wiki/src/App.vue
+++ sw_wiki/src/App.vue
@@ -1,48 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar app color="gray" dark height="100">
-      <div class="d-flex align-center">
-        <v-img
-          alt="R2D2 Logo"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- <v-img src="https://starwarsblog.starwars.com/wp-content/uploads/2020/04/star-wars -backgrounds-14.jpg"> -->
+      <v-app-bar app color="gray" dark height="100">
+        <div class="d-flex align-center">
+          <v-img
+            alt="R2D2 Logo"
+            class="shrink mr-2"
+            contain
+            src="./assets/logo.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <v-img
-          alt="Star Wars Wiki Name"
-          class="shrink mt-1 hidden-sm-and-down d-flex align-start"
-          contain
-          min-width="100"
-          src="./assets/sw_name_logo.png"
-          width="150"
-        />
-      </div>
+          <v-img
+            alt="Star Wars Wiki Name"
+            class="shrink mt-1 hidden-sm-and-down d-flex align-start"
+            contain
+            min-width="100"
+            src="./assets/sw_name_logo.png"
+            width="150"
+          />
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn href="https://swapi.dev/" target="_blank" text>
-        <span class="mr-2">SW API</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-btn href="https://swapi.dev/" target="_blank" text>
+          <span class="mr-2">SW API</span>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-app-bar>
 
-    <v-main>
+      <v-main class="appb">
         <router-view />
-    </v-main>
+      </v-main>
 
-    <v-bottom-navigation v-model="value">
-      <v-btn
-        value="creator"
-        href="https://github.com/igorpserra"
-        target="_blank"
-      >
-        <span>Criado por: @igorpserra</span>
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
+      <v-footer class="appb">
+        <v-bottom-navigation v-model="value" dark>
+          <v-btn
+            value="creator"
+            href="https://github.com/igorpserra"
+            target="_blank"
+          >
+            <span>Criado por: @igorpserra</span>
+            <v-icon>mdi-github</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+      </v-footer>
+    <!--</v-img>-->
   </v-app>
 </template>
 
@@ -55,3 +59,9 @@ export default {
   }),
 };
 </script>
+<style>
+  .appb{
+    background: 
+      url("https://cdn.wallpapersafari.com/81/5/tpObWP.png");
+  }
+</style>
--- sw_wiki/src/components/Carousel.vue
+++ sw_wiki/src/components/Carousel.vue
@@ -1,46 +1,72 @@
 <template>
-  <v-carousel class="mt-3" height="650">
+  <v-carousel class="mt-3" height="400" dark>
     <v-carousel-item
-      v-for="(item,i) in items"
+      v-for="(item, i) in items"
       :key="i"
       :src="item.src"
       reverse-transition="fade-transition"
       transition="fade-transition"
-    ></v-carousel-item>
+      href="/"
+    >
+      <div class="title">
+        <p class="font-title">{{ item.title }}</p>
+      </div>
+    </v-carousel-item>
   </v-carousel>
 </template>
 
 <script>
-  export default {
-    name: 'Carousel',
+export default {
+  name: "Carousel",
 
-    data: () => ({
-      items: [
-          {
-            //Planetas
-            src: 'https://reviewsyouread.files.wordpress.com/2021/03/10-more-star-wars-planets-as-countries.png',
-          },
-          {
-            //Naves
-            src: 'https://media.contentapi.ea.com/content/dam/eacom/en-us/migrated-images/2016/11/news-article-images-ships-blog-image-16x9.jpg',
-          },
-          {
-            //veiculos
-            src: 'https://lrmonline.com/wp-content/uploads/2021/05/Star-WArs-Hoth-2.jpeg',
-          },
-          {
-            //pessoas
-            src: 'https://mlhzuphabiq8.i.optimole.com/AUnRiBM-EI99CML_/w:1024/h:576/q:90/https://pordentrodatela.com.br/wp-content/uploads/2019/11/star-wars-por-dentro-da-tela.jpg',
-          },
-          {
-            //filmes
-            src: "https://cdn.unifiedcommerce.com/content/product/large/079346118045.jpg",
-          },
-          {
-            //especies
-            src: 'https://i0.wp.com/zyteheist.com/wp-content/uploads/2017/12/star-wars-battlefront-species-1280jpg-c74ff0_1280w.jpg?resize=900%2C506&ssl=1',
-          },
-        ],
-      })
-  }
+  data: () => ({
+    items: [
+      {
+        //Planetas
+        src: "https://reviewsyouread.files.wordpress.com/2021/03/10-more-star-wars-planets-as-countries.png",
+        title: "Planetas",
+      },
+      {
+        //Naves
+        src: "https://media.contentapi.ea.com/content/dam/eacom/en-us/migrated-images/2016/11/news-article-images-ships-blog-image-16x9.jpg",
+        title: "Naves",
+      },
+      {
+        //veiculos
+        src: "https://lrmonline.com/wp-content/uploads/2021/05/Star-WArs-Hoth-2.jpeg",
+        title: "Veiculos",
+      },
+      {
+        //pessoas
+        src: "https://mlhzuphabiq8.i.optimole.com/AUnRiBM-EI99CML_/w:1024/h:576/q:90/https://pordentrodatela.com.br/wp-content/uploads/2019/11/star-wars-por-dentro-da-tela.jpg",
+        title: "Pessoas",
+      },
+      {
+        //filmes
+        src: "https://cdn.unifiedcommerce.com/content/product/large/079346118045.jpg",
+        title: "Filmes",
+      },
+      {
+        //especies
+        src: "https://i0.wp.com/zyteheist.com/wp-content/uploads/2017/12/star-wars-battlefront-species-1280jpg-c74ff0_1280w.jpg?resize=900%2C506&ssl=1",
+        title: "Espécies",
+      },
+    ],
+  }),
+};
 </script>
+<style>
+.title {
+  position: absolute;
+  bottom: 50px;
+  color: yellow;
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 20px;
+  border-top-left-radius: 10%;
+  border-top-right-radius: 10%;
+}
+
+.font-title {
+  font-family: "Days One", sans-serif;
+}
+</style>
